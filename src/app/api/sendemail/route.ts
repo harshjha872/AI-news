b156ee9f6b93e443541db8fea5ef24695454ea0e
Harsh Jha <harshjha872@gmail.com>
2024-05-12 12:11:38 +0530

    - corn setup & api filters added

--- src/app/api/sendemail/route.ts
+++ src/app/api/sendemail/route.ts
@@ -1,24 +1,62 @@
 const nodemailer = require("nodemailer");
 import { NextResponse } from "next/server";
+import prisma from "../../../../db/db.config";
+import axios from "axios";
+import { singleNews } from "@/interfaces/interfaces";
 
 export async function GET(request: Request) {
-  // const transporter = nodemailer.createTransport({
-  //   service: "gmail",
-  //   auth: {
-  //     user: "harshjha872",
-  //     pass: process.env.NEXT_PUBLIC_MAIL_PASSWORD,
-  //   },
-  // });
+  try {
+    let news: Array<singleNews>;
+    const allusers = await prisma.subscribers.findMany();
+    const receivers = allusers.map((user) => user.email).join(",");
 
-  // const info = await transporter.sendMail({
-  //   from: '"Daily AI" <dailyainews2024@gmail.com>', // sender address
-  //   to: "playedinindia@gmail.com", // list of receivers
-  //   subject: "Your daily AI News is here", // Subject line
-  //   text: "This is daily ai test", // plain text body
-  //   html: "<b>Hello world?</b>", // html body
-  // });
+    const currentDate = new Date();
 
-  console.log("this is a cron job for testing purpose");
+    const startDate = new Date(currentDate);
+    startDate.setDate(currentDate.getDate() - 2);
 
-  return NextResponse.json({ message: "email is sent successfully" });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "dailyainews2024",
+        pass: process.env.NEXT_PUBLIC_MAIL_PASSWORD,
+      },
+    });
+
+    const res = await axios.get(
+      `https://newsapi.org/v2/everything?q=ai&from=${formatDate(
+        startDate
+      )}&to=${formatDate(currentDate)}&pageSize=15&apiKey=${
+        process.env.NEXT_PUBLIC_NEWSAPI
+      }`
+    );
+
+    news = res.data.articles;
+
+    let newsHtml = "";
+
+    news.forEach((singleNews) => {
+      newsHtml += `<div>${singleNews.description} - ${singleNews.author}<a href="${singleNews.url}" target="_blank"> more</a></div><br>`;
+    });
+
+    const info = await transporter.sendMail({
+      from: '"Daily AI" <dailyainews2024@gmail.com>', // sender address
+      subject: "Your daily AI News is here", // Subject line
+      text: "This is daily ai test", // plain text body
+      html: `<b>Latest AI News</b><br><br>${newsHtml}`, // html body
+      bcc: `${receivers}`,
+    });
+
+    return NextResponse.json({ message: "email is sent successfully" });
+  } catch (err) {
+    console.log("In sending email to subs", err);
+    return NextResponse.json({ message: "something went wrong!" });
+  }
+}
+
+function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,23 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   const [feedNews, setFeedNews] = useState([] as Array<singleNews>);
 
+  const currentDate = new Date();
+
+  const startDate = new Date(currentDate);
+  startDate.setDate(currentDate.getDate() - 2);
+
   useEffect(() => {
     (async function () {
       const res = await axios.get(
-        `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.NEXT_PUBLIC_NEWSAPI}`
+        `https://newsapi.org/v2/everything?q=ai&from=${formatDate(
+          startDate
+        )}&to=${formatDate(currentDate)}&pageSize=15&apiKey=${
+          process.env.NEXT_PUBLIC_NEWSAPI
+        }`
       );
       const data = res.data.articles;
       setFeedNews(data);
     })();
-  }, []);
-
-  useEffect(() => {
-    // fetch("api/sendemail").then(async (res) => {
-    //   const data = await res.json();
-    //   console.log("data ->", data);
-    // });
   }, []);
 
   return (
@@ -35,4 +37,11 @@ export default function Home() {
       {feedNews.length > 0 && <MainFeed News={feedNews} />}
     </>
   );
+}
+
+function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
 }
